cd85efa68f35545e6bd74e56d3748bc2d691ee05
Gabo <gabriel.piles.glez@gmail.com>
2024-09-05 13:02:22 +0200

    Layout analysis container error handling

--- src/extract_segments.py
+++ src/extract_segments.py
@@ -5,6 +5,7 @@ from data_model.ExtractionData import ExtractionData
 from data_model.Task import Task
 import requests
 
+RETRIES = 3
 
 def get_xml_name(task: Task) -> str:
     return f"{task.tenant}__{task.params.filename.lower().replace('.pdf', '.xml')}"
@@ -15,12 +16,20 @@ def extract_segments(task: Task, xml_file_name: str = "") -> ExtractionData:
     data = {"fast": "True"}
     url = DOCUMENT_LAYOUT_ANALYSIS_URL + (f"/save_xml/{xml_file_name}" if xml_file_name else "")
 
-    with open(pdf_file.get_path(task.params.filename), "rb") as stream:
-        files = {"file": stream}
-        results = requests.post(url, files=files, data=data)
+    results = None
+    for i in range(RETRIES):
+        with open(pdf_file.get_path(task.params.filename), "rb") as stream:
+            files = {"file": stream}
+            results = requests.post(url, files=files, data=data)
+
+        if results and results.status_code == 200:
+            break
+
+    if not results:
+        raise SystemError(f"The PDF document layout analysis container did not respond with \n {task}")
 
     if results.status_code != 200:
-        service_logger.error(f"Response error: {results.status_code} - {results.text}")
+        raise RuntimeError(f"Error processing PDF document: {results.status_code} - {results.text}")
 
     segments: list[SegmentBox] = [SegmentBox(**segment_box) for segment_box in results.json()]
     return ExtractionData(
