9a4b95258d559bb8f7ef8ab4c6126fad78f6334f
Gabo <gabriel.piles.glez@gmail.com>
2026-09-15 14:20:57 +0200

    Ask for PDF in local

--- src/use_cases/extract_segments_use_case.py
+++ src/use_cases/extract_segments_use_case.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 from configuration import (
+    DATA_PATH,
     DOCUMENT_LAYOUT_ANALYSIS_URL,
     USE_FAST,
     OCR_OUTPUT,
@@ -56,6 +57,13 @@ def extract_segments(task: Task, xml_file_name: str = "") -> ExtractionData:
     if results.status_code != 200:
         raise RuntimeError(f"Error processing PDF document: {results.status_code} - {results.text}")
 
+    if xml_file_name and not Path(DATA_PATH, xml_file_name).exists():
+        service_logger.info(
+            f"XML file {xml_file_name} is not available in the service. Downloading it from the local service."
+        )
+        if not save_local_xml_file(xml_file_name):
+            raise RuntimeError(f"Error downloading XML file {xml_file_name} from the local service")
+
     segments: list[SegmentBox] = [SegmentBox(**segment_box) for segment_box in results.json()]
     return ExtractionData(
         tenant=task.tenant,
@@ -68,6 +76,23 @@ def extract_segments(task: Task, xml_file_name: str = "") -> ExtractionData:
 
 def extract_segments_cloud(pdf_file: PdfFile, task: Task, xml_file_name: str = "") -> (bool, ExtractionData):
     return CLOUD_ADAPTER.extract_segments(pdf_file, task, xml_file_name)
+
+
+def save_local_xml_file(xml_file_name: str) -> bool:
+    xml_file_path = Path(DATA_PATH, xml_file_name)
+
+    for i in range(RETRIES):
+        try:
+            results = requests.get(f"{DOCUMENT_LAYOUT_ANALYSIS_URL}/get_xml/{xml_file_name}")
+        except Exception as e:
+            service_logger.error(f"Error downloading XML file: {e}")
+            continue
+
+        if results and results.status_code == 200:
+            xml_file_path.write_bytes(results.content)
+            return True
+
+    return False
 
 
 def ocr_pdf(task: Task) -> bool:
